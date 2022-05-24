20f4d27ff93bdefaa7d30d4e19d362cc5b64f6db
Arb <arbrahimbadsa@gmail.com>
2022-05-25 00:55:37 +0600

    cleared

--- src/components/AddScreen.js
+++ src/components/AddScreen.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import styled from "styled-components";
 import Latex from "./Latex";
 import Input from "./Input";
